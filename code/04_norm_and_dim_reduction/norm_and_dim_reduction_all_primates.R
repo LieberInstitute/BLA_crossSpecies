75873e1b00e1c8e2323c008f3bc6d7bb52a60046
Michael Totty <mictott@gmail.com>
2023-12-18 10:14:55 -0500

    updated norm_and_dim_reduction_all_primates.R script locally. Pushing to JHPCE to run remotely.

--- code/04_norm_and_dim_reduction/norm_and_dim_reduction_all_primates.R
+++ code/04_norm_and_dim_reduction/norm_and_dim_reduction_all_primates.R
@@ -6,10 +6,11 @@ library(scater)
 library(scran)
 library(here)
 library(batchelor)
+library(orthogene)
 
 ## save directories
-plot_dir = here("plots", "04_normalization")
-processed_dir = here("processed-data","04_normalization")
+plot_dir = here("plots", "04_norm_and_dim_reduction")
+processed_dir = here("processed-data","04_norm_and_dim_reduction")
 
 # load sce
 load(file=here("processed-data","03_quality_control","Human","sce_post_qc.rda"))
@@ -61,47 +62,135 @@ sce.baboon
 # altExpNames(0):
 
 
-which(seqnames(sce.macaque) == "MT")
+# ====== Subset to only 1:1 orthologs ======
 
-rownames(sce.macaque)[which(seqnames(sce.macaque) == "MT")]
+method <- "gprofiler"
 
-# use grep to get genes with "MT-" in their name in human
-mt_human <- rownames(sce.human)[grep("MT-", rownames(sce.human))]
-mt_human
+baboon_orthos <- orthogene::convert_orthologs(gene_df = counts(sce.baboon),
+                                        gene_input = "rownames", 
+                                        gene_output = "rownames", 
+                                        input_species = "macaque",
+                                        output_species = "human",
+                                        non121_strategy = "drop_both_species",
+                                        method = method) 
+# =========== REPORT SUMMARY ===========
+# Total genes dropped after convert_orthologs :
+#     5,487 / 21,091 (26%)
+# Total genes remaining after convert_orthologs :
+#     15,604 / 21,091 (74%)
 
-# drop MT- from mt_human
-mt_human <- gsub("MT-", "", mt_human)
-mt_human
+macaque_orthos <- orthogene::convert_orthologs(gene_df = counts(sce.macaque),
+                                              gene_input = "rownames", 
+                                              gene_output = "rownames", 
+                                              input_species = "baboon",
+                                              output_species = "human",
+                                              non121_strategy = "drop_both_species",
+                                              method = method) 
+# =========== REPORT SUMMARY ===========
+# Total genes dropped after convert_orthologs :
+#     6,221 / 21,369 (29%)
+# Total genes remaining after convert_orthologs :
+#     15,148 / 21,369 (71%)
+
+nhp_orthologs <- intersect(rownames(baboon_orthos), rownames(macaque_orthos))
+length(nhp_orthologs)
+# [1] 14025
+
+# get only orthologs that are in the human dataset
+valid.human.orthologs <- nhp_orthologs[nhp_orthologs %in% rownames(sce.human)]
+length(valid.human.orthologs)
+# [1] 13948
+
+# get only orthologs that are in the macaque dataset
+valid.macaque.orthologs <- nhp_orthologs[nhp_orthologs %in% rownames(sce.macaque)]
+length(valid.macaque.orthologs)
+# [1] 13952
+
+# get only orthologs that are in the baboon dataset
+valid.baboon.orthologs <- nhp_orthologs[nhp_orthologs %in% rownames(sce.baboon)]
+length(valid.baboon.orthologs)
+# [1] 13937
+
+# get only orthologs common to all datasets
+human_v_mac.orthologs <- intersect(valid.human.orthologs, valid.macaque.orthologs)
+final.121.orthologs <- intersect(human_v_mac.orthologs, valid.baboon.orthologs)
+length(final.121.orthologs)
+# [1] 13874
+
+# subset to only 1:1 orthologs
+sce.human.ortho <- sce.human[final.121.orthologs ,]
+sce.human.ortho
+# class: SingleCellExperiment 
+# dim: 13874 21268 
+# metadata(1): Samples
+# assays(1): counts
+# rownames(13874): ANKRD65 AURKAIP1 ... R3HDM4 KISS1R
+# rowData names(7): source type ... gene_type Symbol.uniq
+# colnames(21268): 1_AAACCCAAGCTAAATG-1 1_AAACCCACAGGTCCCA-1 ...
+# 5_TTTGTTGTCGGACTGC-1 5_TTTGTTGTCGTTGTTT-1
+# colData names(27): Sample Barcode ... discard_auto doubletScore
+# reducedDimNames(0):
+#     mainExpName: NULL
+# altExpNames(0):
+
+sce.macaque.ortho <- sce.macaque[final.121.orthologs,]
+sce.macaque.ortho
+# dim: 13874 109142 
+# metadata(1): Samples
+# assays(1): counts
+# rownames(13874): ANKRD65 AURKAIP1 ... R3HDM4 KISS1R
+# rowData names(7): source type ... gene_biotype Symbol.uniq
+# colnames(109142): 1_AAACGAAAGCGCCGTT-1 1_AAACGAACAGCCGTTG-1 ...
+# 35_TTTGGTTGTGGCTAGA-1 35_TTTGTTGCACATGGTT-1
+# colData names(13): Sample Barcode ... discard_auto doubletScore
+# reducedDimNames(0):
+#     mainExpName: NULL
+# altExpNames(0):
+
+sce.baboon.ortho <- sce.baboon[final.121.orthologs,]
+sce.baboon.ortho
+# class: SingleCellExperiment 
+# dim: 13874 47039 
+# metadata(1): Samples
+# assays(1): counts
+# rownames(13874): ANKRD65 AURKAIP1 ... R3HDM4 KISS1R
+# rowData names(5): ID Symbol Type gene_id_ncbi Symbol.uniq
+# colnames(47039): 1_AAACCCAAGACTGTTC-1 1_AAACCCAAGATCCAAA-1 ...
+# 7_TTTGTTGTCGTGGGAA-1 7_TTTGTTGTCTTAATCC-1
+# colData names(21): Sample Barcode ... discard_auto doubletScore
+# reducedDimNames(0):
+#     mainExpName: NULL
+# altExpNames(0):
+
+# rename variables
+sce.human <- sce.human.ortho
+sce.macaque <- sce.macaque.ortho
+sce.baboon <- sce.baboon.ortho
 
 
-# use grep to get genes with "MT-" in their name in macaque
-mt_macaque <- rownames(sce.macaque)[grep("MT-", rownames(sce.macaque))]
-mt_macaque
+# =========== Quick cluster, compute size factors, and normalize ==============
 
-# use grep to get genes with "MT-" in their name in baboon
-mt_baboon <- rownames(sce.baboon)[grep("MT-", rownames(sce.baboon))]
-mt_baboon
+set.seed(100)
 
-# =========== Calculate size factors ==============
+# normalize human data
+clust.human <- quickCluster(sce.human)
+sce.human <- computeSumFactors(sce.human, cluster=clust.human, min.mean=0.1)
+sce.human <- logNormCounts(sce.human)
 
-human_v_mac <- intersect(rownames(sce.human), rownames(sce.macaque))
-all_common_genes <- intersect(human_v_mac, rownames(sce.baboon))
+# normalize macaque data
+clust.macaque <- quickCluster(sce.macaque)
+sce.macaque <- computeSumFactors(sce.macaque, cluster=clust.macaque, min.mean=0.1)
+sce.macaque <- logNormCounts(sce.macaque)
 
-sce.human <- sce.human[all_common_genes,]
-sce.macaque <- sce.macaque[all_common_genes,]
-sce.baboon <- sce.baboon[all_common_genes,]
+# normalize baboon data
+clust.baboon <- quickCluster(sce.baboon)
+sce.baboon <- computeSumFactors(sce.baboon, cluster=clust.baboon, min.mean=0.1)
+sce.baboon <- logNormCounts(sce.baboon)
 
-dim(sce.human)
-# [1] 20842 21268
-dim(sce.macaque)
-# [1]  14391 109142
-
-# ===== Perform multi-experiment normalization =====
-
-out <- multiBatchNorm(sces = list(sce.human, sce.macaque, sce.baboon))
-sce.human <- out[[1]]
-sce.macaque <- out[[2]]
-sce.baboon <- out[[3]]
+# save sce objects
+saveRDS(sce.human, file = here(processed_dir, "sce.human_normalized.rds"))
+saveRDS(sce.macaque, file = here(processed_dir, "sce.macaque_normalized.rds"))
+saveRDS(sce.baboon, file = here(processed_dir, "sce.baboon_normalized.rds"))
 
 
 # ===== Feature seleection =====
@@ -111,17 +200,25 @@ dec2 <- modelGeneVar(sce.macaque)
 dec3 <- modelGeneVar(sce.baboon)
 
 combined.dec <- combineVar(dec1, dec2, dec3)
-chosen.hvgs <- getTopHVGs(combined.dec, n=5000)
+chosen.hvgs <- getTopHVGs(combined.dec, n=4000)
 
 # As a sanity check, let's confirm that there are indeed batch effects in the data
 
 combined <- correctExperiments(Human=sce.human, Macaque=sce.macaque, Baboon=sce.baboon, PARAM=NoCorrectParam())
 
+rm(sce.human, sce.macaque, sce.baboon)
+
 set.seed(100)
 combined <- runPCA(combined, subset_row=chosen.hvgs,
-                   BPPARAM = BiocParallel::MulticoreParam(workers=20))
-combined <- runTSNE(combined, dimred="PCA",
-                    BPPARAM = BiocParallel::MulticoreParam(workers=20))
+                   #BPPARAM = BiocParallel::MulticoreParam(workers=1))
+
+combined <- runUMAP(combined, dimred="PCA",
+                    #BPPARAM = BiocParallel::MulticoreParam(workers=1)
+                    )
+
+combined <- runTSNE(combined, dimred="PCA"
+                    #BPPARAM = BiocParallel::MulticoreParam(workers=1)
+                    )
 
 pdf(here(plot_dir, "TSNE_batch_effects.pdf"))
 plotTSNE(combined, colour_by="batch")
@@ -139,7 +236,8 @@ combined <- runPCA(combined, ncomponents = 50,
        ntop = 3000,
        exprs_values = "binomial_deviance_residuals",
        scale = TRUE, name = "GLM-PCA_approx",
-       BPPARAM = BiocParallel::MulticoreParam(workers=20))
+       #BPPARAM = BiocParallel::MulticoreParam(workers=20)
+       )
 
 pdf(here(plot_dir, "GLM_PCA_approx.pdf"))
 plotReducedDim(combined, dimred = "GLM-PCA_approx", colour_by = "batch")
@@ -152,12 +250,14 @@ set.seed(1234)
 combined <- runUMAP(combined,
                     dimred = "GLM-PCA_approx",
                     name = "UMAP",
-                    BPPARAM = BiocParallel::MulticoreParam(workers=20))
+                    #BPPARAM = BiocParallel::MulticoreParam(workers=20)
+                    )
 
 combined <- runTSNE(combined,
                     dimred = "GLM-PCA_approx",
                     name = "TSNE",
-                    BPPARAM = BiocParallel::MulticoreParam(workers=20))
+                    #BPPARAM = BiocParallel::MulticoreParam(workers=20)
+                    )
 
 
 
