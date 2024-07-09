e333a37b5719b3cb7b2268b4ef035db35ef63dc5
Michael Totty <mictott@gmail.com>
2024-07-09 12:30:53 -0400

    updated marker genes plots for finding BA vs LA markers v3

--- code/08_species_comparisons/MarkerGenesToValidate.R
+++ code/08_species_comparisons/MarkerGenesToValidate.R
@@ -13,7 +13,6 @@ library(scater)
 processed_dir = here("processed-data", "07_annotation")
 plot_dir = here("plots", "08_species_comparisons", "03_DE_analysis")
 
-sce.inhib <- readRDS(here(processed_dir, "sce.inhib.final.rds"))
 sce.excit <- readRDS(here(processed_dir, "sce.excit.integrated.annotated.rds"))
 
 
@@ -22,7 +21,7 @@ sce.human <- sce.excit[, sce.excit$species == "human"]
 sce.nhp <- sce.excit[, sce.excit$species != "human"]
 
 # Lateral markers
-features <- c("ZBTB20", "ZNF536","GULP1", "HS6ST3", "TSHZ2", "CDH6", "NCALD", "GABRB2", "KCTD8", "AVP")
+features <- c("ZBTB20", "ZNF536","GULP1", "HS6ST3", "TSHZ2", "CDH6", "NCALD", "SATB2", "KCTD8", "AVP")
 png(here(plot_dir, "lateral_markers_human.png"), width=10, height=10, units="in", res=300)
 plotExpression(sce.human, features=features, x="fine_type", color="fine_type", ncol=1) +
     ggtitle("Human: Lateral markers")
