b61bed5804c54c0d479750ce0768e7449b99abcf
Michael Totty <mictott@gmail.com>
2024-07-09 16:29:17 -0400

    reorder factors for better UMAP plotting

--- code/07_annotation/02_plotting_broad_celltypes.R
+++ code/07_annotation/02_plotting_broad_celltypes.R
@@ -12,31 +12,33 @@ library(dplyr)
 library(patchwork)
 library(ggpubr)
 library(RColorBrewer)
+library(forcats)
 
 ## save directories
 plot_dir = here("plots", "07_annotation", "01_broad_annotations")
 
 # load sce
 sce <- readRDS(here("processed-data", "07_annotation", "sce_broad_annotations.rds"))
+sce
 
 # ========== Comparing macaque, baboon, and human cells ========
 
 #subset to different species
-sce_macaque <- sce[,which(colData(sce)$species == "macaque")]
-sce_baboon <- sce[,which(colData(sce)$species == "baboon")]
-sce_human <- sce[,which(colData(sce)$species == "human")]
+# sce_macaque <- sce[,which(colData(sce)$species == "macaque")]
+# sce_baboon <- sce[,which(colData(sce)$species == "baboon")]
+# sce_human <- sce[,which(colData(sce)$species == "human")]
 
-dim(sce)
-# [1]  13842 171928
+# dim(sce)
+# # [1]  13842 171928
 
-dim(sce_macaque)
-# [1]  13842 104797
+# dim(sce_macaque)
+# # [1]  13842 104797
 
-dim(sce_baboon)
-# [1] 13842 46111
+# dim(sce_baboon)
+# # [1] 13842 46111
 
-dim(sce_human)
-# [1] 13842 21020
+# dim(sce_human)
+# # [1] 13842 21020
 
 png(here(plot_dir, "UMAP_across_species.png"), width=12, height=4, units="in", res=300)
 
@@ -161,6 +163,12 @@ region_colors <- c("Basal" = "#E41A1C", "Lateral" = "#377EB8", "Central Nucleus"
 species_colors <- c("human" = "#375E97", "baboon" = "#FB6542", "macaque" = "#6AB187")
 
 png(here(plot_dir, "UMAP_Figure1.png"), width=10, height=10, units="in", res=300)
+
+# reorder to best show mixing across species
+sce$species <- fct_relevel(sce$species, "macaque", "baboon", "human")
+sce <- sce[,order(colData(sce)$species)]
+
+
 p1 <- plotReducedDim(sce, dimred = "UMAP", colour_by = "species", point_size=0.1) +
     scale_color_manual(values = species_colors) +
     theme_void() +
@@ -175,6 +183,11 @@ p1 <- plotReducedDim(sce, dimred = "UMAP", colour_by = "species", point_size=0.1
     legend.title = element_blank()
    ) +
    guides(colour = guide_legend(override.aes = list(size=5, alpha = 1)))
+
+
+# reorder again to best show differences across subregions
+sce$species <- fct_relevel(sce$species, "human", "baboon", "macaque")
+sce <- sce[,order(colData(sce)$species)]
 
 p2 <- plotReducedDim(sce, dimred = "UMAP", colour_by = "Subregion", point_size=0.1) +
     scale_color_manual(values = region_colors) +
