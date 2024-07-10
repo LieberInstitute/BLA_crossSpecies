859683260229a7e6607cc0a22918718065fb6e15
Michael Totty <mictott@gmail.com>
2024-07-09 23:18:00 -0400

    subcluster UMAPs

--- code/07_annotation/subcluster_batch_correction_samples.R
+++ code/07_annotation/subcluster_batch_correction_samples.R
@@ -16,19 +16,23 @@ plot_dir = here("plots", "07_annotation")
 processed_dir = here("processed-data","07_annotation")
 
 
-sce.excit <- readRDS(here("processed-data", "07_annotation", "sce_excit_subclustering.rds"))
-sce.inhib <- readRDS(here("processed-data", "07_annotation", "sce_inhib_subclustering.rds"))
-sce.other <- readRDS(here("processed-data", "07_annotation", "sce_other_subclustering.rds"))
+# load sce
+sce <- readRDS(here("processed-data", "07_annotation", "sce_broad_annotations.rds"))
+sce
 
+
+# subset to broad celltypes in $broad_celltype
+sce.excit <- sce[, sce$broad_celltype == "Excitatory"]
+sce.inhib <- sce[, sce$broad_celltype == "Inhibitory"]
 
 # ========= Conver to Seurat v4 ==========
 
-sce.objects <- list("sce.excit", "sce.inhib", "sce.other")
-names <- c("excit", "inhib", "other")
+sce.objects <- list("sce.excit", "sce.inhib")
+names <- c("excit", "inhib")
 
 for (i in 1:length(sce.objects)) {
 
-    sce <- get(sce.objects[[3]])
+    sce <- get(sce.objects[[i]])
     sce
     
     # drop duplicated colData 
@@ -53,11 +57,11 @@ for (i in 1:length(sce.objects)) {
     saveRDS(seurat, file = paste0(processed_dir, "/seurat_v4/", names[i], "_unintegrated.rds"))
     
     # plot uncorrected data
-    pdf(here(plot_dir, "seurat_subclustering_batch_correction", names[i], "UMAP_species_uncorrected.pdf"))
+    pdf(here(plot_dir, "02_subclustering", names[i], "UMAP_species_uncorrected.pdf"))
     DimPlot(seurat, reduction = "umap.unintegrated", group.by = c("species"))
     dev.off()
     
-    pdf(here(plot_dir, "seurat_subclustering_batch_correction", names[i], "UMAP_samples_uncorrected.pdf"))
+    pdf(here(plot_dir, "02_subclustering", names[i], "UMAP_samples_uncorrected.pdf"))
     DimPlot(seurat, reduction = "umap.unintegrated", group.by = c("Sample")) + NoLegend()
     dev.off()
     
@@ -98,11 +102,11 @@ for (i in 1:length(sce.objects)) {
     saveRDS(seurat.int, file = paste0(processed_dir, "/seurat_v4/", names[i], ".integrated.rds"))
     
     # plot integrated data
-    pdf(here(plot_dir, "seurat_subclustering_batch_correction", names[i], "UMAP_species_integrated.pdf"))
+    pdf(here(plot_dir, "02_subclustering", names[i], "UMAP_species_integrated.pdf"))
     print(DimPlot(seurat.int, reduction = "umap", group.by = c("species")))
     dev.off()
     
-    pdf(here(plot_dir, "seurat_subclustering_batch_correction", names[i], "UMAP_samples_integrated.pdf"))
+    pdf(here(plot_dir, "02_subclustering", names[i], "UMAP_samples_integrated.pdf"))
     print(DimPlot(seurat.int, reduction = "umap", group.by = c("Sample")) + NoLegend())
     dev.off()
 
@@ -114,7 +118,6 @@ for (i in 1:length(sce.objects)) {
 
 seurat.excit <- readRDS(here("processed-data", "07_annotation", "seurat_v4", "excit.integrated.rds"))
 seurat.inhib <- readRDS(here("processed-data", "07_annotation", "seurat_v4", "inhib.integrated.rds"))
-seurat.other <- readRDS(here("processed-data", "07_annotation", "seurat_v4", "other.integrated.rds"))
 
 
 # plot integrated data
@@ -142,93 +145,4 @@ dev.off()
 
 pdf(here(plot_dir, "seurat_subclustering_batch_correction", "other", "UMAP_samples_integrated.pdf"))
 print(DimPlot(seurat.other, reduction = "umap", group.by = c("Sample")) + NoLegend())
-dev.off()
-
-
-
-
-
-# ========================================================
-
-i <- 3
-
-sce <- get(sce.objects[[3]])
-sce
-
-# drop duplicated colData 
-sce <- sce[,!duplicated(colnames(sce))]
-
-seurat <- as.Seurat(sce, counts="counts", data="logcounts")
-
-# standard normalization in seurat
-seurat <- NormalizeData(seurat)
-seurat <- FindVariableFeatures(seurat, nfeatures = 500)
-seurat <- ScaleData(seurat)
-
-# Run PCA
-seurat <- RunPCA(seurat)
-
-# Run UMAP
-seurat <- FindNeighbors(seurat, dims=1:30, reduction="pca")
-seurat <- FindClusters(seurat, resolution=2, cluster.name = "unintegrated_clusters")
-seurat <- RunUMAP(seurat, dims=1:30, reduction="pca", reduction.name = "umap.unintegrated")
-
-# save seurat object
-#saveRDS(seurat, file = paste0(processed_dir, "/seurat_v4/", names[i], "_unintegrated.rds"))
-
-# plot uncorrected data
-# pdf(here(plot_dir, "seurat_subclustering_batch_correction", names[i], "UMAP_species_uncorrected.pdf"))
-# DimPlot(seurat, reduction = "umap.unintegrated", group.by = c("species"))
-# dev.off()
-# 
-# pdf(here(plot_dir, "seurat_subclustering_batch_correction", names[i], "UMAP_samples_uncorrected.pdf"))
-# DimPlot(seurat, reduction = "umap.unintegrated", group.by = c("Sample")) + NoLegend()
-# dev.off()
-
-unique(seurat@meta.data$species)
-# [1] "human"   "baboon"  "macaque"
-
-# split the dataset into a list of seurat objects by species
-seurat.list <- SplitObject(seurat, split.by = "species")
-
-# normalize and identify variable features for each dataset independently
-seurat.list<- lapply(X = seurat.list, FUN = function(x) {
-    x <- NormalizeData(x)
-    x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 500)
-})
-
-# select features that are repeatedly variable across datasets for integration run PCA on each
-# dataset using these features
-features <- SelectIntegrationFeatures(object.list = seurat.list, nfeatures = 500)
-seurat.list <- lapply(X = seurat.list, FUN = function(x) {
-    x <- ScaleData(x, features = features, verbose = FALSE)
-    x <- RunPCA(x, features = features, verbose = FALSE)
-})
-
-# find integration anchors
-seurat.anchors <- FindIntegrationAnchors(object.list = seurat.list, anchor.features = features, reduction = "rpca")
-
-# save integrated seurat object
-#save(seurat.anchors, file = paste0(processed_dir, "/seurat_v4/", names[i], ".anchors.rda"))
-
-# integrate datasets
-seurat.int <- IntegrateData(anchorset = seurat.anchors, dims = 1:30)
-
-# standard seurat workflow for norm and vis
-seurat.int <- ScaleData(seurat.int, verbose = FALSE)
-seurat.int <- RunPCA(seurat.int, npcs = 30, verbose = FALSE)
-seurat.int <- RunUMAP(seurat.int, reduction = "pca", dims = 1:30)
-seurat.int <- FindNeighbors(seurat.int, reduction = "pca", dims = 1:30)
-seurat.int <- FindClusters(seurat.int, resolution = 0.5)
-
-# save integrated seurat object
-#saveRDS(seurat.int, file = paste0(processed_dir, "/seurat_v4/", names[i], ".integrated.rds"))
-
-# plot integrated data
-pdf(here(plot_dir, "seurat_subclustering_batch_correction", names[i], "UMAP_species_integrated_500hvg.pdf"))
-print(DimPlot(seurat.int, reduction = "umap", group.by = c("species")))
-dev.off()
-
-pdf(here(plot_dir, "seurat_subclustering_batch_correction", names[i], "UMAP_samples_integrated_500hvg.pdf"))
-print(DimPlot(seurat.int, reduction = "umap", group.by = c("Sample")) + NoLegend())
 dev.off()
