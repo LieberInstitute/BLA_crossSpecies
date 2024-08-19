36cbcb2e923b59c7e2388b7dae09e9dd9442f655
Michael Totty <mictott@gmail.com>
2024-08-19 13:33:52 -0400

    ran 02_specificity_score.R and 03_bed.R for human BLA cell types

--- code/09_psychiatric_GSEA/LDSC/02_specificity_score.R
+++ code/09_psychiatric_GSEA/LDSC/02_specificity_score.R
@@ -1,7 +1,5 @@
 # originally copied from https://github.com/LieberInstitute/spatialdACC/blob/main/code/17_LDSC/spatial/02_specificity_score.R
 
-setwd('/dcs04/lieber/marmaypag/spatialdACC_LIBD4125/spatialdACC/')
-
 # Load libraries
 library(here)
 
@@ -10,9 +8,9 @@ is_top_10_percent <- function(column) {
     as.numeric(column >= top_10_percent_threshold)
 }
 
-dat <- read.table(here::here("processed-data", "17_LDSC", "visium_aggregated_cpm.tsv"),header=T)
+dat <- read.table(here::here("processed-data", "09_psychiatric_GSEA","LDSC", "human_sce_aggregated.tsv"),header=T)
 
 dat.norm <- t(apply(dat, 1, function(x){x/sum(x)}))
 res <- apply(dat.norm, 2, is_top_10_percent)
 rownames(res) <- rownames(dat.norm)
-write.csv(res,here::here("processed-data", "17_LDSC", "visium_score.csv"))
+write.csv(res,here::here("processed-data", "09_psychiatric_GSEA","LDSC", "human_sce_score.csv"))
