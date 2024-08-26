c633607826e31d7950566fa2bfaf8d7acb072af8
Michael Totty <mictott@gmail.com>
2024-08-26 17:12:09 -0400

    LDSC on NHP samples

--- code/08_species_and_celltype_comparisons/05_dotplots_genes_of_interest.R
+++ code/08_species_and_celltype_comparisons/05_dotplots_genes_of_interest.R
@@ -5,6 +5,8 @@ library(SingleCellExperiment)
 library(ggplot2)
 library(scater)
 library(dplyr)
+library(scuttle)
+library(tidyr)
 
 
 # directories
@@ -18,7 +20,7 @@ sce.inhib <- readRDS(here("processed-data", "07_annotation_and_characterization"
 
 
 
-# ==== add putative subregion labels ====
+# ================ Excitatory ===============
 
 # combine putative LA, BA, and aBA clusters
 LA_clusters <- c("GULP1_TRHDE", "ZBTB20_SLC4A4")
@@ -33,86 +35,177 @@ unique(sce.excit$subregion_celltype)
 # [1] "aBA"   "BA"    "Other" "LA"   
 
 
-# genes <- c(
-#     "GRM1", "GRM5", "GRM2", "GRM3", "GRM4", "GRM6", "GRM7", "GRM8",
-#     "GRIA1", "GRIA2", "GRIA3", "GRIA4", "GRIK1", "GRIK2", "GRIK3", "GRIK4", "GRIK5",
-#     "GRIN1", "GRIN2A", "GRIN2B", "GRIN2C", "GRIN2D", "GRIN3A", "GRIN3B",
-#     "HTR1A", "HTR1B", "HTR1D", "HTR1E", "HTR2A", "HTR2C", "HTR4", "HTR5A", "HTR6", "HTR7",
-#     "DRD1", "DRD2", "DRD3", "DRD4", "DRD5",
-#     "CHRNA3", "CHRNA5", "CHRNA6", "CHRNB3", "CHRNB4",
-#     "ADRA1A", "ADRA1B", "ADRA2A", "ADRA2C", "ADRB1", "ADRB2", "AVPR1A", "AVPR1B", "AVPR2", "CNR1", "CNR2", 
-#     "DRD2", "DRD3", "F2RL1", "F2RL2", "F2RL3", "GHRHR", "HCRTR2", "KISS1R", "MC2R", "MC3R", "MC4R", "MC5R", 
-#     "NPBWR1", "NPBWR2", "NPY1R", "NPY2R", "NPY4R", "NPY5R", "OPRD1", "OPRK1", "OPRM1", "OXTR", "PRLHR", 
-#     "PTGER1", "PTGER2", "PTGER3", "PTGER4", "PTH1R", "PTH2R", "SSTR1", "SSTR2", "SSTR3", "SSTR4", "SSTR5", 
-#     "TACR1", "TACR2", "TACR3", "TRHR", "V1AR", "V1BR"
-# )
+# ======== Faceting around all receptr classes =========
 
-
+# Define receptor classes and their corresponding genes
 serotonin_genes <- c("HTR1A", "HTR1B", "HTR1D", "HTR1E", "HTR2A", "HTR2C", "HTR4", "HTR5A", "HTR6", "HTR7")
-
+opioid_genes <- c("OPRM1", "OPRD1", "OPRK1", "OPRL1")
 dopamine_genes <- c("DRD1", "DRD2", "DRD3", "DRD4", "DRD5")
-
 norepinephrine_genes <- c("ADRA1A", "ADRA1B", "ADRA2A", "ADRA2C", "ADRB1", "ADRB2")
-
 cannabinoid_genes <- c("CNR1", "CNR2")
+#cholinergic_genes <- c("CHRNA3", "CHRNA5", "CHRNA6", "CHRNB3", "CHRNB4")
+muscarinic_genes <- c(
+  "CHRNA1", "CHRNA2", "CHRNA3", "CHRNA4", "CHRNA5", "CHRNA6", "CHRNA7", "CHRNA9", "CHRNA10",
+  "CHRNB1", "CHRNB2", "CHRNB3", "CHRNB4", 
+  "CHRND", "CHRNE", "CHRNG",
+  "CHRM1", "CHRM2", "CHRM3", "CHRM4", "CHRM5"
+)
 
-cholinergic_genes <- c("CHRNA3", "CHRNA5", "CHRNA6", "CHRNB3", "CHRNB4")
+# keep only in sce
+serotonin_genes <- serotonin_genes[serotonin_genes %in% rownames(sce.excit)]
+opioid_genes <- opioid_genes[opioid_genes %in% rownames(sce.excit)]
+dopamine_genes <- dopamine_genes[dopamine_genes %in% rownames(sce.excit)]
+norepinephrine_genes <- norepinephrine_genes[norepinephrine_genes %in% rownames(sce.excit)]
+cannabinoid_genes <- cannabinoid_genes[cannabinoid_genes %in% rownames(sce.excit)]
+cholinergic_genes <- cholinergic_genes[cholinergic_genes %in% rownames(sce.excit)]
 
 
+# Assign receptor class labels to each gene
+receptor_class <- c(
+  rep("Serotonin", length(serotonin_genes)),
+  rep("Opioid", length(opioid_genes)),
+  rep("Dopamine", length(dopamine_genes)),
+  rep("Norepinephrine", length(norepinephrine_genes)),
+  rep("CBRs", length(cannabinoid_genes)),
+  rep("Acetylcholine", length(cholinergic_genes))
+)
 
-# drop any not in sce.excit
-genes <- genes[genes %in% rownames(sce.excit)]
+# Create a data frame to map genes to their receptor class
+gene_receptor_map <- data.frame(gene = all_genes, receptor_class = receptor_class)
 
-# drop any duplicate
-genes <- unique(genes)
 
-# excitatory
-png(here(plot_dir, "dotplot_excitatory_receptrs_and_such.png"), width = 20, height = 5, units = "in", res = 300)
-plotDots(sce.excit, features=serotonin_genes, group="fine_celltype", block="species", center=TRUE, scale=TRUE) +
-    coord_flip() +
-    scale_color_gradient2(low = "blue", mid = "white", high = "red") +
-    # x label horizontal
-    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-     facet_wrap("subregion_celltype"~., scales = "free_y") 
+# Aggregate expression data across all defined genes
+sce.excit_pseudo <- aggregateAcrossCells(
+  sce.excit, 
+  ids = sce.excit$fine_celltype, 
+  subset_row = all_genes,
+  statistics = "mean", 
+  use.assay.type = "logcounts"
+)
+
+  # Extract expression data from the SingleCellExperiment object
+expr_data <- as.data.frame(assay(sce.excit_pseudo, "logcounts"))
+
+# Add gene and fine cell type information
+expr_data$gene <- rownames(expr_data)
+expr_data_long <- expr_data %>%
+  pivot_longer(cols = -gene, names_to = "fine_celltype", values_to = "expression")
+
+# Merge with receptor class information
+expr_data_long <- merge(expr_data_long, gene_receptor_map, by = "gene")
+
+# Extract subcluster information from colData
+subcluster_info <- colData(sce.excit_pseudo) %>% as.data.frame()
+subcluster_info$fine_celltype <- rownames(subcluster_info)
+
+# Merge the expression data with subcluster information
+plot_data <- merge(expr_data_long, subcluster_info, by = "fine_celltype", all.x = TRUE)
+
+
+# Create a dot plot using ggplot2
+png(here(plot_dir, "dotplot_excit_receptor_classes.png"), width = 15, height = 5, units = "in", res = 300)
+ggplot(plot_data, aes(x = gene, y = fine_celltype, fill = expression)) +
+  geom_tile() +
+  scale_fill_gradient(low = "white", high = "red") +
+  theme_minimal() +
+  labs(x = "Gene", y = "Fine Cell Type", color = "Scaled Expression") +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    strip.background = element_rect(fill = "grey80", color = "grey50"),
+    strip.text = element_text(face = "bold")
+  ) +
+  facet_grid(subregion_celltype ~ receptor_class, scales = "free", space = "free")  # Facet by receptor class and subregion
 dev.off()
 
 
-# inhibitory
-png(here(plot_dir, "dotplot_inhibitory_receptrs_and_such.png"), width = 20, height = 5, units = "in", res = 300)
-plotDots(sce.inhib, features=genes, group="fine_celltype", block="species", center=TRUE, scale=TRUE) +
-    coord_flip() +
-    scale_color_gradient2(low = "blue", mid="white", high = "red") +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-    labs(color="centered.scaled") 
+
+
+
+
+# ================ Inhibitory ===============
+
+
+# ======== Faceting around all receptr classes =========
+
+# Define receptor classes and their corresponding genes
+serotonin_genes <- c("HTR1A", "HTR1B", "HTR1D", "HTR1E", "HTR2A", "HTR2C", "HTR4", "HTR5A", "HTR6", "HTR7")
+opioid_genes <- c("OPRM1", "OPRD1", "OPRK1", "OPRL1")
+dopamine_genes <- c("DRD1", "DRD2", "DRD3", "DRD4", "DRD5")
+norepinephrine_genes <- c("ADRA1A", "ADRA1B", "ADRA2A", "ADRA2C", "ADRB1", "ADRB2")
+cannabinoid_genes <- c("CNR1", "CNR2")
+#cholinergic_genes <- c("CHRNA3", "CHRNA5", "CHRNA6", "CHRNB3", "CHRNB4")
+muscarinic_genes <- c(
+  "CHRNA1", "CHRNA2", "CHRNA3", "CHRNA4", "CHRNA5", "CHRNA6", "CHRNA7", "CHRNA9", "CHRNA10",
+  "CHRNB1", "CHRNB2", "CHRNB3", "CHRNB4", 
+  "CHRND", "CHRNE", "CHRNG",
+  "CHRM1", "CHRM2", "CHRM3", "CHRM4", "CHRM5"
+)
+
+# keep only in sce
+serotonin_genes <- serotonin_genes[serotonin_genes %in% rownames(sce.excit)]
+opioid_genes <- opioid_genes[opioid_genes %in% rownames(sce.excit)]
+dopamine_genes <- dopamine_genes[dopamine_genes %in% rownames(sce.excit)]
+norepinephrine_genes <- norepinephrine_genes[norepinephrine_genes %in% rownames(sce.excit)]
+cannabinoid_genes <- cannabinoid_genes[cannabinoid_genes %in% rownames(sce.excit)]
+cholinergic_genes <- cholinergic_genes[cholinergic_genes %in% rownames(sce.excit)]
+
+
+# Assign receptor class labels to each gene
+receptor_class <- c(
+  rep("Serotonin", length(serotonin_genes)),
+  rep("Opioid", length(opioid_genes)),
+  rep("Dopamine", length(dopamine_genes)),
+  rep("Norepinephrine", length(norepinephrine_genes)),
+  rep("CBRs", length(cannabinoid_genes)),
+  rep("Acetylcholine", length(cholinergic_genes))
+)
+
+# Create a data frame to map genes to their receptor class
+gene_receptor_map <- data.frame(gene = all_genes, receptor_class = receptor_class)
+
+
+# Aggregate expression data across all defined genes
+sce.excit_pseudo <- aggregateAcrossCells(
+  sce.inhib, 
+  ids = sce.inhib$fine_celltype, 
+  subset_row = all_genes,
+  statistics = "mean", 
+  use.assay.type = "logcounts"
+)
+
+  # Extract expression data from the SingleCellExperiment object
+expr_data <- as.data.frame(assay(sce.excit_pseudo, "logcounts"))
+
+# Add gene and fine cell type information
+expr_data$gene <- rownames(expr_data)
+expr_data_long <- expr_data %>%
+  pivot_longer(cols = -gene, names_to = "fine_celltype", values_to = "expression")
+
+# Merge with receptor class information
+expr_data_long <- merge(expr_data_long, gene_receptor_map, by = "gene")
+
+# Extract subcluster information from colData
+subcluster_info <- colData(sce.excit_pseudo) %>% as.data.frame()
+subcluster_info$fine_celltype <- rownames(subcluster_info)
+
+# Merge the expression data with subcluster information
+plot_data <- merge(expr_data_long, subcluster_info, by = "fine_celltype", all.x = TRUE)
+
+
+# Create a dot plot using ggplot2
+png(here(plot_dir, "dotplot_inhib_receptor_classes.png"), width = 15, height = 5, units = "in", res = 300)
+ggplot(plot_data, aes(x = gene, y = fine_celltype, fill = expression)) +
+  geom_tile() +
+  scale_fill_gradient(low = "white", high = "red") +
+  theme_minimal() +
+  labs(x = "Gene", y = "Fine Cell Type", color = "Scaled Expression") +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    strip.background = element_rect(fill = "grey80", color = "grey50"),
+    strip.text = element_text(face = "bold")
+  ) +
+  facet_grid( ~receptor_class, scales = "free", space = "free")  # Facet by receptor class and subregion
 dev.off()
-
-
-
-
-# Aggregate mean expression across cells by fine_celltype
-agg_data <-scuttle::aggregateAcrossCells(
-    sce.excit,
-    ids = sce.excit$fine_celltype, # Aggregate by fine_celltype
-    statistics = "mean",
-    assay.type="logcounts",
-    subset.row=serotonin_genes
-)
-
-
-
-# Create a data frame for gene categories
-gene_categories <- data.frame(
-    gene = c(serotonin_genes, dopamine_genes, norepinephrine_genes, cannabinoid_genes, cholinergic_genes),
-    category = c(rep("Serotonin", length(serotonin_genes)),
-                 rep("Dopamine", length(dopamine_genes)),
-                 rep("Norepinephrine", length(norepinephrine_genes)),
-                 rep("Cannabinoid", length(cannabinoid_genes)),
-                 rep("Cholinergic", length(cholinergic_genes)))
-)
-
-# Add the expression data from the sce.excit object (assuming it's in a tidy format)
-
-
 
 
 
